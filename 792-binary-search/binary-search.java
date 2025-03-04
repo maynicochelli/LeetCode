dbb47feab52c713e8d0b576648f4a33024b8cd9a
May Nicochelli <58159670+maynicochelli@users.noreply.github.com>
2025-03-04 11:29:30 -0300

    Time: 0 ms (100.00%) | Memory: 45.7 MB (76.43%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -4,7 +4,7 @@ class Solution {
         int right = nums.length - 1;
 
         while (left <= right) {
-            int mid = right - left / 2;
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] > target) {
