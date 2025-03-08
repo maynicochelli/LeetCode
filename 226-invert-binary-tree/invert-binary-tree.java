f38c76d96de45350d7f8b3265d589c88edcda43c
May Nicochelli <58159670+maynicochelli@users.noreply.github.com>
2025-03-07 23:23:06 -0300

    Time: 0 ms (100.00%) | Memory: 41.1 MB (77.85%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.java
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -22,10 +22,8 @@ class Solution {
         TreeNode temp = root.left;
         root.left = root.right;
         root.right = temp;
-
         invertTree(root.left);
         invertTree(root.right);
-
         return root;
     }
 }
