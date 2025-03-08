5a05993e610cbcd4fb6cf8cfc6bd65d71a03f500
May Nicochelli <58159670+maynicochelli@users.noreply.github.com>
2025-03-07 21:47:19 -0300

    Time: 1 ms (89.72%) | Memory: 44.6 MB (98.90%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -1,28 +1,28 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> levels = new ArrayList<List<Integer>>(); // return List
+        List<List<Integer>> levels = new ArrayList<List<Integer>>();
         if (root == null) {
             return levels;
         }
         Deque<TreeNode> queue = new ArrayDeque<>();
         queue.addLast(root);
         int level = 0;
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             levels.add(new ArrayList<Integer>());
             int size = queue.size();
             for (int i = 0; i < size; i++) {
