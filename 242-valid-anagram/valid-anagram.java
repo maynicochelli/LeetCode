cfd20270cabf1b80ec8dd0d1fab30f907253e73b
May Nicochelli <58159670+maynicochelli@users.noreply.github.com>
2025-03-12 21:04:52 -0300

    Time: 14 ms (29.52%) | Memory: 45 MB (17.99%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
-
         HashMap<Character, Integer> sMap = new HashMap<>();
         HashMap<Character, Integer> tMap = new HashMap<>();
         for (char c: s.toCharArray()) {
