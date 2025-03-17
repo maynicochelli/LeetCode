69cd66f36297e91a8524f2f586b2a32f74baeddb
May Nicochelli <58159670+maynicochelli@users.noreply.github.com>
2025-03-17 13:08:24 -0300

    Time: 0 ms (100.00%) | Memory: 46 MB (33.52%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -2,7 +2,7 @@ class Solution {
     public int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-        while (left <= right) {
+        while(left <= right) {
             int mid = left + (right - left) / 2;
             if (target == nums[mid]) {
                 return mid;
@@ -11,7 +11,7 @@ class Solution {
             } else {
                 right = mid - 1;
             }
-        }   
-        return -1;     
+        }
+        return -1;
     }
 }
